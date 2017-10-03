a951322bb9289573445fb14fa7bfe88fcaaf198f
Kai Hotz <kaihotz@me.com>
2017-10-03 10:44:14 +0200

    removed unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import App from './components/app'
 import reducers from './reducers'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducers, composeEnhancers(
+  applyMiddleware(reduxThunk)
+))
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>
   , document.querySelector('.container'))
