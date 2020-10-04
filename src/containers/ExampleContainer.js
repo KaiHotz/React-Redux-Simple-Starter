40b88190ff3d5a41ca08297a70a948d4354123a8
Kai Hotz <kaihotz@me.com>
2020-10-04 07:05:20 +0400

    adjustment

--- src/containers/ExampleContainer.js
+++ src/containers/ExampleContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch } from 'react-redux'
 import { connect } from 'react-redux'
